64740eefc258cbad68060daff615375f6e1dfcd6
Adam <adam.marton@digitalasset.com>
2023-03-20 13:43:26 +0100

    Fix index generation [PLEN-200 fix] (#10)
    * Helm index generation fixed

--- scripts/chart_publish.sh
+++ scripts/chart_publish.sh
@@ -30,7 +30,7 @@ for chart in $CHARTS_DIR/*; do
         helm package $chart -d $PUBLISH_FOLDER
         cr upload -b https://api.github.com/ -u https://uploads.github.com --skip-existing -c $PUBLISH_BRANCH -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
         echo "Chart $chart_name version $chart_version has been pushed to repository $GITREPO_NAME"
-        helm repo index publish --url https://github.com/adamma-da/test-chart-releaser/releases/download/$chart_name-$chart_version/  --merge docs/index.yaml
+        helm repo index publish --url https://github.com/$GITREPO_OWNER/$GITREPO_NAME/releases/download/$chart_name-$chart_version/  --merge docs/index.yaml
 
     fi
 done
